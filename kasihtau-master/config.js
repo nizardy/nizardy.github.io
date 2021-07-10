ded83abe584cee31ce0651718c6da39d72f0a860
Ardyy Ghaffar <64342401+ghaffardy@users.noreply.github.com>
2021-07-10 11:10:52 +0800

    Add files via upload

---
+++ kasihtau-master/config.js
@@ -0,0 +1,9 @@
+/*
+  * This file is part of KasihTau Project which is released under MIT License.
+  * See file LICENSE or go to https://github.com/shidoitsuka/kasihtau/blob/master/LICENSE for full license details.
+*/
+
+module.exports = {
+  host: "192.168.0.105",
+  port: "3000",
+};
