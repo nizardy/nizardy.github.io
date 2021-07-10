b1998f4f1112f261ab0ff7f9d75c6749ba4b439d
Ardyy Ghaffar <64342401+ghaffardy@users.noreply.github.com>
2021-07-10 19:06:45 +0800

    Delete kasihtau-master directory

--- kasihtau-master/config.js
+++
@@ -1,9 +0,0 @@
-/*
-  * This file is part of KasihTau Project which is released under MIT License.
-  * See file LICENSE or go to https://github.com/shidoitsuka/kasihtau/blob/master/LICENSE for full license details.
-*/
-
-module.exports = {
-  host: "192.168.0.105",
-  port: "3000",
-};
