23df02dbcf24a5e7edb78e787fefe65e1881bbe9
Ardyy Ghaffar <64342401+ghaffardy@users.noreply.github.com>
2021-07-12 03:49:13 +0800

    Add files via upload

---
+++ iloveu/nuxt.config.js
@@ -0,0 +1,91 @@
+import colors from 'vuetify/es5/util/colors'
+
+export default {
+  /*
+  ** Nuxt rendering mode
+  ** See https://nuxtjs.org/api/configuration-mode
+  */
+  mode: 'universal',
+  /*
+  ** Nuxt target
+  ** See https://nuxtjs.org/api/configuration-target
+  */
+  target: 'server',
+  /*
+  ** Headers of the page
+  ** See https://nuxtjs.org/api/configuration-head
+  */
+  head: {
+    // titleTemplate: '%s - ' + process.env.npm_package_name,
+    title: "Brian Harianja.",
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: "description", name: "description", content: "Hi, I'm Brian. A full-Stack developer from Sidoarjo!" },
+      { hid: "og:description", name: "og:description", content: "Hi, I'm Brian. A full-Stack Developer from Sidoarjo!" },
+      { hid: "og:image", name: "og:image", content: "/img/photo.JPG" },
+      { hid: "og:title", name: "og:title", content: "Brian Harianja" }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  ** https://nuxtjs.org/guide/plugins
+  */
+  plugins: [
+  ],
+  /*
+  ** Auto import components
+  ** See https://nuxtjs.org/api/configuration-components
+  */
+  components: true,
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+    '@nuxtjs/vuetify',
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+  ],
+  /*
+  ** vuetify module configuration
+  ** https://github.com/nuxt-community/vuetify-module
+  */
+  vuetify: {
+    customVariables: ['~/assets/variables.sass'],
+    theme: {
+      dark: false,
+      themes: {
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+        }
+      }
+    }
+  },
+  /*
+  ** Build configuration
+  ** See https://nuxtjs.org/api/configuration-build/
+  */
+  server: {
+    host: "localhost",
+    port: 3000
+  },
+  build: {
+  }
+}
