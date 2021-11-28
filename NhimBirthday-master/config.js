2659d3b9f2a30621d88747cde2761dec702e0cea
Ardyy Ghaffar <64342401+ghaffardy@users.noreply.github.com>
2021-11-29 03:05:56 +0800

    Add files via upload

---
+++ NhimBirthday-master/config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "Nhím's birthday",
+    introDesc: `Nghe giang hồ đồn hôm nay là sinh nhật của Phan Lê Nhím, nên anh muốn làm cái gì đó tặng em nè hehe,
+     món quà vật chất bằng 0%, nhưng mà tui hi zọng là em sẽ thích nó kaka, em biết tui làm gì cũng tâm huyết mà đúng hông? 😙`,
+    btnIntro: '^^HiHi^^',
+    title: 'Trả lời đúng câu này sẽ được quà nè ^^',
+    desc: 'Sợ anh khônggg?? 😙',
+    btnYes: 'Gất là sợ luônnn :>>',
+    btnNo: 'Không nha :3',
+    question: 'Đấy, ngoan như thế mới được,giờ em muốn quà gì, ông già Đô-el sẽ gửi cho em 🎅🎅😉',
+    btnReply: '📧',
+    reply: 'Em lớn rồi nên hông cần quà đâu hihi',
+    mess: 'Trêu tý thôi hehe 😙',
+    messDesc: 'Em xứng đáng có 10 hộp quà ^^.',
+    btnAccept: '👉🎁🎁🎁👈',
+    messLink: 'http://localhost:63342/app/1Date.html?_ijt=a4rkifg1c8g8b685jphb08re87' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
