e5f5f94718753aeb4fb6e377827fe3bd5d016db9
Ardyy Ghaffar <64342401+ghaffardy@users.noreply.github.com>
2021-11-29 03:14:06 +0800

    Add files via upload

---
+++ cobalagi/config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Phương Táo';
+const giftUrl = 'https://www.youtube.com/watch?v=_nRxlQBcR_U';
+const eventName = 'Happy Birth Day';
+const titleCard = 'Tặng Phương Táo';
+const contentCard = 'Chúc bạn Táo có một ngày sinh nhật thật là vui vẻ và hạnh phúc ❤️❤️❤️';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
