9dff41512ad594f263ce90294421584e917639a0
Ardyy Ghaffar <64342401+ghaffardy@users.noreply.github.com>
2022-02-13 23:46:47 +0800

    Update main.js

--- han-birthday/script/main.js
+++ han-birthday/script/main.js
@@ -1,3 +1,10 @@
+var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+  if (!isChrome){
+      $('#iframeAudio').remove()
+  }
+  else {
+      $('#playAudio').remove() // just to make sure that it will not have 2x audio in the background 
+  }
 // Animation Timeline
 const animationTimeline = () => {
   // Spit chars that needs to be animated individually
